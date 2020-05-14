68c2db4109612e34e04afae9ea285a2526fe9920
ValentinaLV <tina.lysenok@gmail.com>
2020-05-14 18:17:56 +0300

    created user app with login and logout templates

--- learning_platform/urls.py
+++ learning_platform/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import BaseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', BaseView.as_view(), name='home_page'),
+    path('user/', include(('user.urls', 'user'), namespace='user')),
 
 ]
 
